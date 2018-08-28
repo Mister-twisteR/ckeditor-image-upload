e6e1b06408af770c60208855e987665c7920c384
roma <curlyne@gmail.com>
2018-08-28 14:17:54 +0300

    added object with editor options

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script src="EditorOptions.js"></script>
     <script src="ckeditor.js"></script>
     <title>CKEditor 5</title>
 </head>
@@ -10,11 +11,74 @@
 <textarea name="content" id="editor"></textarea>
 
 <script>
+
+    let editorOptions = {};
+
+    /*
+   * Below is a list of default editor options. To disable one just put "//" comment left to the title
+   */
+
+    editorOptions.options = [
+        'heading',         // h1, h2, h3 tag
+        '|',               // it is just a section divider
+        'bold',            // bold font
+        'italic',          // italic font
+        'underline',       // underline font
+        'strikethrough',   // strikethrough font
+        'code',            // code styling tag
+        'highlight',       // to highlight some text samples
+        '|',
+        'imageUpload',     // imageUpload function
+        'link',            // link insertion
+        'bulletedList',    // bulleted list tag
+        'numberedList',    // numbered list tag
+        'blockQuote',      // blockQuote tag
+        '|',
+        'alignment',       // text alignment
+        'fontFamily',      // font family choice
+        'fontSize',        // font size choice
+        '|',
+        'undo',            // undo function
+        'redo',            // redo function
+        '|',
+        'insertTable',     // insert table function
+        'tableColumn',     // insert table column
+        'tableRow',        // insert table row
+        'mergeTableCells', // merge table cells
+    ];
+
     ClassicEditor.create(document.querySelector( '#editor' ), {
+        toolbar: editorOptions.options,
+        fontFamily: {
+            options: [
+                'default',
+                'Arial, Helvetica, sans-serif',
+                'Courier New, Courier, monospace',
+                'Georgia, serif',
+                'Lucida Sans Unicode, Lucida Grande, sans-serif',
+                'Tahoma, Geneva, sans-serif',
+                'Times New Roman, Times, serif',
+                'Trebuchet MS, Helvetica, sans-serif',
+                'Verdana, Geneva, sans-serif',
+                'Ubuntu, Arial, sans-serif',
+                'Ubuntu Mono, Courier New, Courier, monospace'
+            ]
+        },
+        fontSize: {
+            options: [
+                9,
+                11,
+                13,
+                'default',
+                17,
+                19,
+                21
+            ]
+        },
         simpleUpload: {
-            uploadUrl: 'http://ckeditor/upload.php'
+            uploadUrl: 'http://<?= $_SERVER['HTTP_HOST']; ?>/upload.php'
         }
-    })
+    });
 </script>
 
 </body>
